039eeecf4b71909d05117b9fd813b2f8eca0c0c6
Pai Peng <sipaipv6@gmail.com>
2023-11-25 10:14:29 +0800

    bluetooth slavi serial connection

--- cp_esp32_car.ino
+++ cp_esp32_car.ino
@@ -1,5 +1,21 @@
 #include "cp_car.h"
 
+#include <BluetoothSerial.h>
+//#define USE_PIN // Uncomment this to use PIN during pairing. The pin is specified on the line below
+const char *pin = "1211";
+
+String device_name = "CP_IOT_CAR";
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+#if !defined(CONFIG_BT_SPP_ENABLED)
+#error Serial Bluetooth not available or not enabled. It is only available for the ESP32 chip.
+#endif
+
+BluetoothSerial SerialBT;
+
 // Motor Driver 0: back
 #define DRIVER_0_IN1_PIN 27  // The ESP32 pin GPIO27 connected to the IN1 pin L298N
 #define DRIVER_0_IN2_PIN 26  // The ESP32 pin GPIO26 connected to the IN2 pin L298N
@@ -21,6 +37,16 @@ void setup() {
 
   Serial.println("Serial inited");
   delay(2000);
+  Serial.printf("The device with name \"%s\" is started.\nNow you can pair it with Bluetooth!\n", device_name.c_str());
+//Serial.printf("The device with name \"%s\" and MAC address %s is started.\nNow you can pair it with Bluetooth!\n", device_name.c_str(), SerialBT.getMacString()); // Use this after the MAC method is implemented
+#ifdef USE_PIN
+  SerialBT.setPin(pin);
+  Serial.println("Using PIN");
+#endif
+
+
+
+
 
 
   motor_driver_pin front_driver_pin;
@@ -41,5 +67,13 @@ void setup() {
 }
 
 void loop() {
-  car.forward();
+  //car.forward();
+
+  if (Serial.available()) {
+    SerialBT.write(Serial.read());
+  }
+  if (SerialBT.available()) {
+    Serial.write(SerialBT.read());
+  }
+  delay(20);
 }
